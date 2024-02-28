269b1f5cd9b9884461b27f30902186c36d3325a2
MatteoDZ <61154534+MatteoDZ@users.noreply.github.com>
2024-02-28 16:55:23 +0100

    Added one compression test

---
+++ src/test/java/it/unipi/dii/aide/mircv/index/compression/CompressionTest.java
@@ -0,0 +1,19 @@
+package it.unipi.dii.aide.mircv.index.compression;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompressionTest {
+    
+    @BeforeAll
+    void setUp() {}
+
+    @Test
+    void VariableByteTest() {
+        assertEquals(VariableByteCompressor.encode([824, 5]), [6, 184, 133])
+    }
+}
