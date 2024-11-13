2971b1cc5c09f5a013a26b64fbd611a6b3bbefa6
Leonardo <leonardo.bargiotti@stud.unifi.it>
2024-11-13 20:30:16 +0100

    refactor tests

--- src/test/java/it/unipi/dii/aide/mircv/query/QueryPerformanceEvaluationMainTest.java
+++ src/test/java/it/unipi/dii/aide/mircv/query/QueryPerformanceEvaluationMainTest.java
@@ -43,75 +43,58 @@ public class QueryPerformanceEvaluationMainTest {
 
         assertEquals(List.of(1, 5, 3, 6, 8, 2),
                 getRankedQuery(Processer.processQuery("atomic bomb", 6, true, "tfidf", false, false)));
-        assertEquals(List.of(1, 5, 3, 6, 8, 2),
+        assertEquals(List.of(1, 8, 5, 2, 6, 3),
                 getRankedQuery(Processer.processQuery("atomic bomb", 6, true, "bm25", false, false)));
         assertEquals(List.of(5, 1, 2, 3, 8, 6),
                 getRankedQuery(Processer.processQuery("atomic bomb", 6, false, "tfidf", false, false)));
-        assertEquals(List.of(5, 1, 2, 3, 8, 6),
+        assertEquals(List.of(1, 8, 5, 2, 6, 3),
                 getRankedQuery(Processer.processQuery("atomic bomb", 6, false, "bm25", false, false)));
 
         assertEquals(List.of(1, 5, 3, 6, 8, 2),
                 getRankedQuery(Processer.processQuery("atomic bomb", 6, true, "tfidf", false, true)));
-        assertEquals(List.of(1, 5, 3, 6, 8, 2),
+        assertEquals(List.of(1, 8, 5, 2, 6, 3),
                 getRankedQuery(Processer.processQuery("atomic bomb", 6, true, "bm25", false, true)));
         assertEquals(List.of(5, 1, 2, 3, 8, 6),
                 getRankedQuery(Processer.processQuery("atomic bomb", 6, false, "tfidf", false, true)));
-        assertEquals(List.of(5, 1, 2, 3, 8, 6),
+        assertEquals(List.of(1, 8, 5, 2, 6, 3),
                 getRankedQuery(Processer.processQuery("atomic bomb", 6, false, "bm25", false, true)));
 
         assertEquals(List.of(45, 42, 43, 47, 44, 41, 48, 46, 40, 39),
                 getRankedQuery(Processer.processQuery("costa rica", 10, true, "tfidf", false, false)));
-        assertEquals(List.of(45, 42, 43, 47, 44, 41, 48, 46, 40, 39),
+        assertEquals(List.of(42, 47, 45, 40, 43, 41, 44, 48, 46, 39),
                 getRankedQuery(Processer.processQuery("costa rica", 10, true, "bm25", false, false)));
         assertEquals(List.of(45, 42, 43, 47, 44, 41, 48, 46, 40, 39),
                 getRankedQuery(Processer.processQuery("costa rica", 10, false, "tfidf", false, false)));
-        assertEquals(List.of(45, 42, 43, 47, 44, 41, 48, 46, 40, 39),
+        assertEquals(List.of(42, 47, 45, 40, 43, 41, 44, 48, 46, 39),
                 getRankedQuery(Processer.processQuery("costa rica", 10, false, "bm25", false, false)));
 
         assertEquals(List.of(45, 42, 43, 47, 44, 41, 48, 46, 40, 39),
                 getRankedQuery(Processer.processQuery("costa rica", 10, true, "tfidf", false, true)));
-        assertEquals(List.of(45, 42, 43, 47, 44, 41, 48, 46, 40, 39),
+        assertEquals(List.of(42, 47, 45, 40, 43, 41, 44, 48, 46, 39),
                 getRankedQuery(Processer.processQuery("costa rica", 10, true, "bm25", false, true)));
         assertEquals(List.of(45, 42, 43, 47, 44, 41, 48, 46, 40, 39),
                 getRankedQuery(Processer.processQuery("costa rica", 10, false, "tfidf", false, true)));
-        assertEquals(List.of(45, 42, 43, 47, 44, 41, 48, 46, 40, 39),
+        assertEquals(List.of(42, 47, 45, 40, 43, 41, 44, 48, 46, 39),
                 getRankedQuery(Processer.processQuery("costa rica", 10, false, "bm25", false, true)));
-
-        assertEquals(List.of(2, 7, 3, 5, 4, 6, 8, 1, 9, 0),
-                getRankedQuery(Processer.processQuery("Manhattan project", 10, true, "tfidf", false, false)));
-        assertEquals(List.of(2, 7, 3, 5, 4, 6, 8, 1, 9, 0),
-                getRankedQuery(Processer.processQuery("Manhattan project", 10, true, "bm25", false, false)));
-        assertEquals(List.of(2, 7, 3, 5, 4, 6, 8, 1, 9, 0),
-                getRankedQuery(Processer.processQuery("Manhattan project", 10, false, "tfidf", false, false)));
-        assertEquals(List.of(2, 7, 3, 5, 4, 6, 8, 1, 9, 0),
-                getRankedQuery(Processer.processQuery("Manhattan project", 10, false, "bm25", false, false)));
-
-        assertEquals(List.of(2, 7, 3, 5, 4, 6, 8, 1, 9, 0),
-                getRankedQuery(Processer.processQuery("Manhattan project", 10, true, "tfidf", false, true)));
-        assertEquals(List.of(2, 7, 3, 5, 4, 6, 8, 1, 9, 0),
-                getRankedQuery(Processer.processQuery("Manhattan project", 10, true, "bm25", false, true)));
-        assertEquals(List.of(2, 7, 3, 5, 4, 6, 8, 1, 9, 0),
-                getRankedQuery(Processer.processQuery("Manhattan project", 10, false, "tfidf", false, true)));
-        assertEquals(List.of(2, 7, 3, 5, 4, 6, 8, 1, 9, 0),
-                getRankedQuery(Processer.processQuery("Manhattan project", 10, false, "bm25", false, true)));
 
         assertEquals(List.of(3, 6, 7, 1),
                 getRankedQuery(Processer.processQuery("war II", 4, true, "tfidf", false, false)));
-        assertEquals(List.of(3, 6, 7, 1),
+        assertEquals(List.of(1, 6, 34, 3),
                 getRankedQuery(Processer.processQuery("war II", 4, true, "bm25", false, false)));
         assertEquals(List.of(3, 6, 7, 1),
                 getRankedQuery(Processer.processQuery("war II", 4, false, "tfidf", false, false)));
-        assertEquals(List.of(3, 6, 7, 1),
+        assertEquals(List.of(1, 6, 34, 3),
                 getRankedQuery(Processer.processQuery("war II", 4, false, "bm25", false, false)));
 
         assertEquals(List.of(3, 6, 7, 1),
                 getRankedQuery(Processer.processQuery("war II", 4, true, "tfidf", false, true)));
-        assertEquals(List.of(3, 6, 7, 1),
+        assertEquals(List.of(1, 6, 34, 3),
                 getRankedQuery(Processer.processQuery("war II", 4, true, "bm25", false, true)));
         assertEquals(List.of(3, 6, 7, 1),
                 getRankedQuery(Processer.processQuery("war II", 4, false, "tfidf", false, true)));
-        assertEquals(List.of(3, 6, 7, 1),
+        assertEquals(List.of(1, 6, 34, 3),
                 getRankedQuery(Processer.processQuery("war II", 4, false, "bm25", false, true)));
+
     }
 
     // Query results using collection_subset_top100.tar.gz
@@ -121,74 +104,56 @@ public class QueryPerformanceEvaluationMainTest {
 
         assertEquals(List.of(1, 5, 3, 6, 8, 2),
                 getRankedQuery(Processer.processQuery("atomic bomb", 6, true, "tfidf", true, false)));
-        assertEquals(List.of(1, 5, 3, 6, 8, 2),
+        assertEquals(List.of(1, 8, 5, 2, 6, 3),
                 getRankedQuery(Processer.processQuery("atomic bomb", 6, true, "bm25", true, false)));
         assertEquals(List.of(5, 1, 2, 3, 8, 6),
                 getRankedQuery(Processer.processQuery("atomic bomb", 6, false, "tfidf", true, false)));
-        assertEquals(List.of(5, 1, 2, 3, 8, 6),
+        assertEquals(List.of(1, 8, 5, 2, 6, 3),
                 getRankedQuery(Processer.processQuery("atomic bomb", 6, false, "bm25", true, false)));
 
         assertEquals(List.of(1, 5, 3, 6, 8, 2),
                 getRankedQuery(Processer.processQuery("atomic bomb", 6, true, "tfidf", true, true)));
-        assertEquals(List.of(1, 5, 3, 6, 8, 2),
+        assertEquals(List.of(1, 8, 5, 2, 6, 3),
                 getRankedQuery(Processer.processQuery("atomic bomb", 6, true, "bm25", true, true)));
         assertEquals(List.of(5, 1, 2, 3, 8, 6),
                 getRankedQuery(Processer.processQuery("atomic bomb", 6, false, "tfidf", true, true)));
-        assertEquals(List.of(5, 1, 2, 3, 8, 6),
+        assertEquals(List.of(1, 8, 5, 2, 6, 3),
                 getRankedQuery(Processer.processQuery("atomic bomb", 6, false, "bm25", true, true)));
 
         assertEquals(List.of(45, 42, 43, 47, 44, 41, 48, 46, 40, 39),
                 getRankedQuery(Processer.processQuery("costa rica", 10, true, "tfidf", true, false)));
-        assertEquals(List.of(45, 42, 43, 47, 44, 41, 48, 46, 40, 39),
+        assertEquals(List.of(42, 47, 45, 40, 43, 41, 44, 48, 46, 39),
                 getRankedQuery(Processer.processQuery("costa rica", 10, true, "bm25", true, false)));
         assertEquals(List.of(45, 42, 43, 47, 44, 41, 48, 46, 40, 39),
                 getRankedQuery(Processer.processQuery("costa rica", 10, false, "tfidf", true, false)));
-        assertEquals(List.of(45, 42, 43, 47, 44, 41, 48, 46, 40, 39),
+        assertEquals(List.of(42, 47, 45, 40, 43, 41, 44, 48, 46, 39),
                 getRankedQuery(Processer.processQuery("costa rica", 10, false, "bm25", true, false)));
 
         assertEquals(List.of(45, 42, 43, 47, 44, 41, 48, 46, 40, 39),
                 getRankedQuery(Processer.processQuery("costa rica", 10, true, "tfidf", true, true)));
-        assertEquals(List.of(45, 42, 43, 47, 44, 41, 48, 46, 40, 39),
+        assertEquals(List.of(42, 47, 45, 40, 43, 41, 44, 48, 46, 39),
                 getRankedQuery(Processer.processQuery("costa rica", 10, true, "bm25", true, true)));
         assertEquals(List.of(45, 42, 43, 47, 44, 41, 48, 46, 40, 39),
                 getRankedQuery(Processer.processQuery("costa rica", 10, false, "tfidf", true, true)));
-        assertEquals(List.of(45, 42, 43, 47, 44, 41, 48, 46, 40, 39),
+        assertEquals(List.of(42, 47, 45, 40, 43, 41, 44, 48, 46, 39),
                 getRankedQuery(Processer.processQuery("costa rica", 10, false, "bm25", true, true)));
-
-        assertEquals(List.of(2, 7, 3, 5, 4, 6, 8, 1, 9, 0),
-                getRankedQuery(Processer.processQuery("Manhattan project", 10, true, "tfidf", true, false)));
-        assertEquals(List.of(2, 7, 3, 5, 4, 6, 8, 1, 9, 0),
-                getRankedQuery(Processer.processQuery("Manhattan project", 10, true, "bm25", true, false)));
-        assertEquals(List.of(2, 7, 3, 5, 4, 6, 8, 1, 9, 0),
-                getRankedQuery(Processer.processQuery("Manhattan project", 10, false, "tfidf", true, false)));
-        assertEquals(List.of(2, 7, 3, 5, 4, 6, 8, 1, 9, 0),
-                getRankedQuery(Processer.processQuery("Manhattan project", 10, false, "bm25", true, false)));
-
-        assertEquals(List.of(2, 7, 3, 5, 4, 6, 8, 1, 9, 0),
-                getRankedQuery(Processer.processQuery("Manhattan project", 10, true, "tfidf", true, true)));
-        assertEquals(List.of(2, 7, 3, 5, 4, 6, 8, 1, 9, 0),
-                getRankedQuery(Processer.processQuery("Manhattan project", 10, true, "bm25", true, true)));
-        assertEquals(List.of(2, 7, 3, 5, 4, 6, 8, 1, 9, 0),
-                getRankedQuery(Processer.processQuery("Manhattan project", 10, false, "tfidf", true, true)));
-        assertEquals(List.of(2, 7, 3, 5, 4, 6, 8, 1, 9, 0),
-                getRankedQuery(Processer.processQuery("Manhattan project", 10, false, "bm25", true, true)));
 
         assertEquals(List.of(3, 6, 7, 1),
                 getRankedQuery(Processer.processQuery("war II", 4, true, "tfidf", true, false)));
-        assertEquals(List.of(3, 6, 7, 1),
+        assertEquals(List.of(1, 6, 34, 3),
                 getRankedQuery(Processer.processQuery("war II", 4, true, "bm25", true, false)));
         assertEquals(List.of(3, 6, 7, 1),
                 getRankedQuery(Processer.processQuery("war II", 4, false, "tfidf", true, false)));
-        assertEquals(List.of(3, 6, 7, 1),
+        assertEquals(List.of(1, 6, 34, 3),
                 getRankedQuery(Processer.processQuery("war II", 4, false, "bm25", true, false)));
 
         assertEquals(List.of(3, 6, 7, 1),
                 getRankedQuery(Processer.processQuery("war II", 4, true, "tfidf", true, true)));
-        assertEquals(List.of(3, 6, 7, 1),
+        assertEquals(List.of(1, 6, 34, 3),
                 getRankedQuery(Processer.processQuery("war II", 4, true, "bm25", true, true)));
         assertEquals(List.of(3, 6, 7, 1),
                 getRankedQuery(Processer.processQuery("war II", 4, false, "tfidf", true, true)));
-        assertEquals(List.of(3, 6, 7, 1),
+        assertEquals(List.of(1, 6, 34, 3),
                 getRankedQuery(Processer.processQuery("war II", 4, false, "bm25", true, true)));
     }
 }
