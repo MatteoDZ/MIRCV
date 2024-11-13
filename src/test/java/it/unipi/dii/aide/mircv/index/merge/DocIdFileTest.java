994fede35214bbbbf547736f6ec4374edb92f67d
Leonardo <leonardo.bargiotti@stud.unifi.it>
2024-11-13 14:19:50 +0100

    refactor DocIdFileTest

--- src/test/java/it/unipi/dii/aide/mircv/index/merge/DocIdFileTest.java
+++ src/test/java/it/unipi/dii/aide/mircv/index/merge/DocIdFileTest.java
@@ -19,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DocIdFileTest {
 
     List<Integer> docIds = List.of(1, 20, 300, 401, 450, 461, 500, 6000, 70000, 800000, 8000000, 8800000);
+    List<Integer> lenghts = List.of(1, 3 , 4 ,5 , 6, 7, 8, 9, 10, 11, 12, 13);
     FileChannel fcDocIds;
 
     @BeforeEach
@@ -32,14 +33,17 @@ public class DocIdFileTest {
     @Test
      void writeDocIdsTest() throws IOException {
         DocIdFile doc = new DocIdFile();
-        Pair<Long, Integer> offsets = doc.writeBlock(docIds,  false);
+        Pair<Long, Integer> offsets = doc.writeBlock(docIds, lenghts, false);
         fcDocIds = FileChannel.open(Paths.get(Configuration.PATH_DOCID), StandardOpenOption.READ);
-        MappedByteBuffer mmbDocIds = fcDocIds.map(FileChannel.MapMode.READ_ONLY, offsets.getValue0(), 4L *docIds.size());
+        MappedByteBuffer mmbDocIds = fcDocIds.map(FileChannel.MapMode.READ_ONLY, offsets.getValue0(), 4L *docIds.size() + 4L * lenghts.size());
         List<Integer> docIdsRead = new ArrayList<>();
+        List<Integer> lenghtsRead = new ArrayList<>();
         for (int i = 0; i < docIds.size(); i++) {
             docIdsRead.add(mmbDocIds.getInt());
+            lenghtsRead.add(mmbDocIds.getInt());
         }
         assertEquals(docIds, docIdsRead);
+        assertEquals(lenghts, lenghtsRead);
     }
 
 
